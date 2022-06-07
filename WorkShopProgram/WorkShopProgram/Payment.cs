7dcd9b3f85e99760c8249f478a9677861364c919
Arshad <mohammedarshad183@gmail.com>
2022-06-07 09:32:56 +0530

    Added: WorkShop program

---
+++ WorkShopProgram/WorkShopProgram/Payment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkShopProgram
+{
+    class Payment
+    {
+        public double principle;
+        public double year;
+        public double rate;
+        public Payment(double principle, double year, double rate)
+        {
+            this.principle = principle;
+            this.year = year;
+            this.rate = rate;
+        }
+
+        public void Calculate()
+        {
+            double n = this.year * 12;
+            double r = this.rate / (12 * 100);
+            double data = this.principle * r;
+            double Power = Math.Pow(1 + r, -n);
+            double value = (1 - Power);
+            double calculation = data / value;
+            Console.WriteLine(calculation);
+
+
+        }
+    }
+}
---
+++ WorkShopProgram/WorkShopProgram/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WorkShopProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Principle");
+            double principle = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Year");
+            double year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Rate");
+            double Rate = Convert.ToInt32(Console.ReadLine());
+            Payment payment = new Payment(principle,year,Rate);
+            payment.Calculate();
+        }
+    }
+}
